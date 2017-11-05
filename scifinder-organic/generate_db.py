3d81fa27bd18d5418d75c4d7def0ab35877ad2a5
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2017-11-05 17:34:32 -0400

    Add a few more synonyms

--- scifinder-organic/generate_db.py
+++ scifinder-organic/generate_db.py
@@ -1,5 +1,6 @@
 #! /home/caleb/.anaconda3/bin/python
 import sys
+import os
 
 import rdkit
 from rdkit import Chem
@@ -8,9 +9,30 @@ from rdkit.Chem.rdMolDescriptors import CalcMolFormula
 from pubchempy import get_compounds, Compound
 import json
 from collections import Counter
+from thermo import serialize_formula
+
+
+os.system('python2 parse_pdf.py')
+
+
+
+from fcache.cache import FileCache
+mycache = FileCache('myapp', flag='cs', serialize=True, app_cache_dir='/home/caleb/Documents/University/CHE3123/chemical-metadata/fcache')
+
+
+syn_data = open('Good synoynms by CAS.json').read()
+syn_data = json.loads(syn_data)
+
+all_user_names = []
+for CAS, d in syn_data.items():
+    if 'synonyms' in d:
+        all_user_names.extend(d['synonyms'])
+all_user_names = set(all_user_names)
+
 
 pdf_data = open('Parsed scifinder metadata.json').read()
 pdf_data = json.loads(pdf_data)
+
 all_names = []
 for CAS, d in pdf_data.items():
     if 'Other Names' in d:
@@ -25,42 +47,116 @@ for CAS, d in pdf_data.items():
 dup_names =  [item for item, count in Counter(all_names).items() if count > 1]
 all_names = set(all_names)
 
-# TODO finish this
-# Big database = hard to work on
-# Small database = good to work on
-failed_mol = set(['11062-77-4',# No charge
-                  ])
+# TODO add without smiles or inchi
+ignored_CASs = ['12339-27-4',  # H2P2+ can't draw it, not in any db
+                '68111-10-4' # AuBr2+ bromine charge problem, smiles is [Br-][Au+][Br-]
+                # H2P2-1 can't draw it not in any db
+                ]
 
-arg = sys.argv[0:]
-arg.pop(0)
-for f in arg:
-#    try:
-    mol = Chem.MolFromMolFile(f)
-    if mol is None:
-        print('BAILING ON %s'%f)
-        continue
-    smi = Chem.MolToSmiles(mol, True)
-    inchi_val = inchi.MolToInchi(mol)
-    inchikey = inchi.InchiToInchiKey(inchi_val)
-    mw = Descriptors.ExactMolWt(mol)
-    formula = CalcMolFormula(mol)
+args = sys.argv[0:]
+args.pop(0)
+dest = args.pop(-1)
+
+INCLUDE_EVERYTHING = True
+
+if INCLUDE_EVERYTHING:
+    for CAS, d in syn_data.items():
+        if 'pubchem' in d or 'formula' in d:
+            if not any([CAS in i for i in args]):
+                args.append('mol/%s.mol' %CAS)
+
+dest_open = open(dest + '_tmp', 'w')
+print = lambda x : dest_open.write(x+'\n')
+
+def parse_f(f):
+    names = ['']
+    cid = -1
     CAS = f.split('/')[1] if '/' in f else f
     CAS = CAS.split('.')[0]
-    
+    if CAS in ignored_CASs:
+        return None
+    failed_mol = False
     try:
-        pc = get_compounds(inchikey, 'inchikey')[0]
-        cid = pc.cid
-        iupac_name = pc.iupac_name
-        names = pc.synonyms
+        if CAS in syn_data:
+            d = syn_data[CAS]
+            if 'pubchem' in d:
+                raise Exception('Pubchem specified, not trying to use the mol file')
+            elif 'formula' in d:
+                raise Exception('Formula specified, not trying to use the mol file')
+        try:
+            mol = Chem.MolFromMolFile(f)
+            assert mol is not None
+        except:
+            print('Cannot read %s' % f)
+            1/0
+        try:
+            inchi_val = inchi.MolToInchi(mol)
+        except:
+            print('BAILING ON %s' %f)
+            1/0
+        mol = inchi.MolFromInchi(inchi_val) # Works better for ions
+        if mol is None:
+            print('BAILING ON reconversion to mol %s'%f)
+            1/0
+    except:
+        failed_mol = True
+        if CAS in syn_data:
+            d = syn_data[CAS]
+            if 'pubchem' in d:
+                if str(d['pubchem']) in mycache:
+                    cid, iupac_name, names, mw, smi, inchi_val, inchikey, formula = mycache[str(d['pubchem'])]
+                else:
+                    pc = Compound.from_cid(d['pubchem'])
+                    cid = pc.cid
+                    iupac_name = pc.iupac_name
+                    names = pc.synonyms
+                    mw = pc.molecular_weight
+                    smi = pc.canonical_smiles
+                    inchi_val = pc.inchi
+                    inchikey = pc.inchikey
+                    formula = pc.molecular_formula
+                    
+                    mycache[str(d['pubchem'])] = (cid, iupac_name, names, mw, smi, inchi_val, inchikey, formula)
+            else:
+                cid = -1
+                names = d['synonyms'] if 'synonyms' in d else ['']
+                mw = float(d['MW'])
+                smi = d['smiles'] if 'smiles' in d else ''
+                formula = d['formula'] if 'formula' in d else ''
+                inchi_val = d['inchi'] if 'inchi' in d else ''
+                inchikey = d['inchikey'] if 'inchikey' in d else ''
+                iupac_name = ''
+        else:
+            print('FAILED on %s and no custom data was available either' %CAS)
+            return None
+                
+    if not failed_mol:
+        smi = Chem.MolToSmiles(mol, True)
+        inchi_val = inchi.MolToInchi(mol)
+        inchikey = inchi.InchiToInchiKey(inchi_val)
+        mw = Descriptors.MolWt(mol)
+        formula = CalcMolFormula(mol, True, True)
+        iupac_name = ''
+    try:
+        if not failed_mol:
+            if str(inchikey) in mycache:
+                cid, iupac_name, names = mycache[str(inchikey)]
+            else:
+                try:
+                    pc = get_compounds(inchikey, 'inchikey')[0]
+                    cid = pc.cid
+                    iupac_name = pc.iupac_name
+                    names = pc.synonyms
+                    mycache[str(inchikey)] = (cid, iupac_name, names)
+                except:
+                    mycache[str(inchikey)] = (-1, '', [''])
     except:
         cid = -1
         iupac_name = ''
         names = ['']
     
     other_CAS = []
-#    print(CAS, CAS in pdf_data)
     if CAS in pdf_data:
-#        print('hi')
         d = pdf_data[CAS]
         name = d['Name']
         if 'Other Names' in d:
@@ -78,13 +174,38 @@ for f in arg:
             
         syns = [i for i in syns if i not in dup_names]
         names =  syns + [i for i in names if i not in all_names] + other_CAS
-    
+    actual_names = []
+    for name in names:
+        if name in all_user_names:
+            # If the name is in the user db, only add it if it corresponds to this CAS number
+            if CAS in syn_data and 'synonyms' in syn_data[CAS] and name in syn_data[CAS]['synonyms']:
+                actual_names.append(name)
+            else:
+                # Discard it otherwise
+                pass
+        else:
+            # If the name is not in the user db we're all good
+            actual_names.append(name)
+    if CAS in syn_data and 'synonyms' in syn_data[CAS]:
+        # If the user has any syns for this cas number, add those names if the name hasn't already been aded
+        for n in syn_data[CAS]['synonyms']:
+            if n not in actual_names:
+                actual_names.append(n)
 
+    actual_names = [i for i in actual_names if i]
+    
+    formula = serialize_formula(formula)
     s = '%d\t%s\t%s\t%g\t%s\t%s\t%s\t%s\t' %(cid, CAS, formula, mw, smi, inchi_val, inchikey, iupac_name)
-    s += '\t'.join(names)
+    
+    
+    s += '\t'.join(actual_names)
     print(s)
-#    except:
-#        pass
-    
-    
-#4	78-96-6	C3H9NO	75.10966	CC(CN)O	C3H9NO/c1-3(5)2-4/h3,5H,2,4H2,1H3	HXKKHQJGJAFBHI-UHFFFAOYSA-N	1-azanylpropan-2-ol	1-amino-2-propanol	
+    return None
+
+for f in args:
+    parse_f(f)
+
+dest_open.close()
+
+os.system('cat %s | sort -n > %s' %(dest + '_tmp', dest))
+os.remove(dest + '_tmp')
